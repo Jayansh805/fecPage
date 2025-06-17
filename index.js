f7dd9812ec7c58a24012e22a59cc25c77d9cc757
Jayansh <jdgupta1980@gmail.com>
2025-06-17 15:28:36 +0530

    updated

--- index.js
+++ index.js
@@ -32,4 +32,33 @@ document.addEventListener('DOMContentLoaded', () => {
             txt.style.width = "0px";
         })
     });
+
+    const cards = document.querySelectorAll('.card');
+    const suppl = document.querySelector('.image-card');
+    const q_cards = document.querySelectorAll('.quiz-card');
+    const q_suppl = document.querySelector('.quiz-image-card');
+
+    if(cards.length%3==0){
+        suppl.style.display="none";
+    }
+    else if(cards.length%3==1){
+        suppl.style.gridColumn ="span 2";
+    }
+    else{
+        suppl.style.gridColumn="span 1";
+
+        const image = suppl.querySelector('img');
+        image.style.height = "100%";
+    }
+
+
+    if(q_cards.length%3==0){
+        q_suppl.style.display="none";
+    }
+    else if(q_cards.length%3==1){
+        q_suppl.style.gridColumn ="span 1";
+    }
+    else{
+        q_suppl.style.gridColumn="span 1";
+    }
 });
